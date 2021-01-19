0316842db51c3c272a2b6e6882546791703fd809
naz0806 <62138890+naz0806@users.noreply.github.com>
2021-01-19 23:20:21 +0800

    Update login-charity.php

--- login-charity.php
+++ login-charity.php
@@ -8,7 +8,7 @@ $c_email = $c_pass = $c_pass1 = '';
 
 $c_email = mysqli_real_escape_string($link, $_POST['c_email']);
 $c_pass1 = mysqli_real_escape_string($link, $_POST['c_pass1']);
-$c_pass = MD5($d_pass1);
+$c_pass = MD5($c_pass1);
 
 $sql = "SELECT * FROM charity WHERE c_email='$c_email' AND c_pass='$c_pass'";
 $result = mysqli_query($link, $sql);
