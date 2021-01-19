f7117eb445a6155ff530d9df37c15af3848b9395
naz0806 <62138890+naz0806@users.noreply.github.com>
2021-01-19 23:17:42 +0800

    Update login-charity.php

--- login-charity.php
+++ login-charity.php
@@ -10,17 +10,17 @@ $c_email = mysqli_real_escape_string($link, $_POST['c_email']);
 $c_pass1 = mysqli_real_escape_string($link, $_POST['c_pass1']);
 $c_pass = MD5($d_pass1);
 
-$sql = "SELECT * FROM charity WHERE c_email='$d_email' AND c_pass='$d_pass'";
+$sql = "SELECT * FROM charity WHERE c_email='$c_email' AND c_pass='$c_pass'";
 $result = mysqli_query($link, $sql);
 if(mysqli_num_rows($result) > 0)
 {
 	while($row = mysqli_fetch_assoc($result))
 	{
-		$d_id = $row["c_id"];
-		$d_email = $row["c_email"];
+		$c_id = $row["c_id"];
+		$c_email = $row["c_email"];
 		session_start();
-		$_SESSION['c_id'] = $d_id;
-		$_SESSION['c_email'] = $d_email;
+		$_SESSION['c_id'] = $c_id;
+		$_SESSION['c_email'] = $c_email;
 	}
 	header("Location: https://donatemate-cat300.herokuapp.com/charity-homepage.php");
 }
