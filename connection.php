ed13fa28455fe133833f2a6750835d9419cc2238
naz0806 <62138890+naz0806@users.noreply.github.com>
2021-01-19 18:48:59 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 
 <?php
 //connect to the database
-$link = mysqli_connect('localhost', 'root', '', 'donatemate') or die($link);
+$link = mysqli_connect('remotemysql.com', 'ZBJKVx0aFi', 'nmtIUSsxqS', 'ZBJKVx0aFi') or die($link);
 
 if($link->connect_error){
     die('Connection Failed : '.$mysqli->connect_error);
