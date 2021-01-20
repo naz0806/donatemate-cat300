372434f7608cb1e1618117339f90018d5017f70d
hazwani-droid <62138878+hazwani-droid@users.noreply.github.com>
2021-01-20 13:32:56 +0800

    Update deliveryform-code.php

--- deliveryform-code.php
+++ deliveryform-code.php
@@ -11,15 +11,32 @@
         $name = mysqli_real_escape_string($link, $_POST['name']);
         $contact = mysqli_real_escape_string($link, $_POST['contact']);
         $email = mysqli_real_escape_string($link, $_POST['email']);
+}
 
-        
-     
-
-      
-    }
+$sql = "SELECT  don_id FROM donation limit 1";
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
        
-        $sql = "INSERT INTO delivery (role_type, del_name, del_contact, del_email)
-                VALUES ('$role', '$name', '$contact' ,'$email')";
+        while($row = mysqli_fetch_array($result)){
+           
+             $donID =  $row['don_id'] ;
+                            
+           
+        }
+       
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+
+
+       
+        $sql = "INSERT INTO delivery (don_id, role_type, del_name, del_contact, del_email)
+                VALUES ('$donID','$role', '$name', '$contact' ,'$email')";
         
  $result = mysqli_query($link, $sql);
 
